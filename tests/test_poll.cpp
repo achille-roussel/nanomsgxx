f00315bdaa0a9d7762045189a83c23b2a87702f5
Achille Roussel <ach@fb.com>
2014-04-29 11:33:54 -0700

    add comments on poll test

--- tests/test_poll.cpp
+++ tests/test_poll.cpp
@@ -16,12 +16,16 @@ int main() {
   s2.connect("inproc://test");
 
   try {
+    // Poll for sockets that are ready for output operations, both s1 and s2
+    // should be set.
     events = nnxx::poll({{ s1, nnxx::EV_POLLOUT }, { s2, nnxx::EV_POLLOUT }});
     r1 = nnxx::recv_ready(events);
     r2 = nnxx::send_ready(events);
     nnxx_assert(std::distance(r1.begin(), r1.end()) == 0);
     nnxx_assert(std::distance(r2.begin(), r2.end()) == 2);
 
+    // Send a message on s1 and poll s2 for input operations, s1 should be set
+    // and there should be no socket ready for output operations.
     s1.send("Hello World!");
     events = nnxx::poll({{ s2, nnxx::EV_POLLIN }}, std::chrono::seconds(0));
 
@@ -37,21 +41,25 @@ int main() {
       (void)e;
     }
 
+    // Poll for input operations on s2 which has already been flushed, so we
+    // should get a timeout there.
     try {
       nnxx::poll({{ s2, nnxx::EV_POLLIN }}, std::chrono::milliseconds(10));
-      nnxx_assert(false); // we should have timed-out
+      nnxx_assert(false);
     }
     catch (const nnxx::timeout_error &) {
     }
   }
+
+  // None of these conditions should happen.
   catch (const nnxx::signal_error &) {
-    nnxx_assert(false); // shouldn't happen
+    nnxx_assert(false);
   }
   catch (const nnxx::timeout_error &) {
-    nnxx_assert(false); // shuoldn't happen
+    nnxx_assert(false);
   }
   catch (const std::exception &) {
-    nnxx_assert(false); // shouldn't happen
+    nnxx_assert(false);
   }
 
   return nnxx::unittest::result;
